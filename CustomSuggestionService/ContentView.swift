cb22d609501955b0a5614593cb38c8a9ed15b328
Shx Guo <int123c@gmail.com>
2024-01-19 01:02:34 +0800

    Initial Commit

---
+++ CustomSuggestionService/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomSuggestionService
+//
+//  Created by Shangxin Guo on 2024/1/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomSuggestionService/CustomSuggestionServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSuggestionServiceApp.swift
+//  CustomSuggestionService
+//
+//  Created by Shangxin Guo on 2024/1/19.
+//
+
+import SwiftUI
+
+@main
+struct CustomSuggestionServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
