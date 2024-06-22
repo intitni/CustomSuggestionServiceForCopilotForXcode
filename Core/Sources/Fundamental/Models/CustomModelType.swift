565d802872c87700764d51532adb4899cd05845a
Shx Guo <int123c@gmail.com>
2024-06-23 03:12:01 +0800

    Reorder items

--- Core/Sources/Fundamental/Models/CustomModelType.swift
+++ Core/Sources/Fundamental/Models/CustomModelType.swift
@@ -3,8 +3,8 @@ import Foundation
 public enum CustomModelType: String, CaseIterable {
     case chatModel
     case completionModel
-    case tabby
     case fimModel
+    case tabby
     
     public static var `default`: CustomModelType {
         .completionModel
