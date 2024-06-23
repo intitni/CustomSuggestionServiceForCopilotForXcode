229b479ae1a7e8b9d346cc8c6caf3cabc338f199
Shx Guo <int123c@gmail.com>
2024-06-23 16:31:42 +0800

    Update CopilotForXcodeKit

--- CopilotForXcodeExtension/CopilotForXcodeExtension.swift
+++ CopilotForXcodeExtension/CopilotForXcodeExtension.swift
@@ -4,10 +4,7 @@ import SuggestionService
 
 @main
 class Extension: CopilotForXcodeExtension {
-    var host: HostServer?
-    var suggestionService: SuggestionServiceType?
-    var chatService: ChatServiceType? { nil }
-    var promptToCodeService: PromptToCodeServiceType? { nil }
+    let suggestionService = SuggestionService()
     var sceneConfiguration = SceneConfiguration()
 
     let updateChecker =
@@ -15,11 +12,6 @@ class Extension: CopilotForXcodeExtension {
             hostBundle: locateHostBundleURL(url: Bundle.main.bundleURL)
                 .flatMap(Bundle.init(url:))
         )
-
-    required init() {
-        let service = SuggestionService()
-        suggestionService = service
-    }
 }
 
 struct SceneConfiguration: CopilotForXcodeExtensionSceneConfiguration {}
